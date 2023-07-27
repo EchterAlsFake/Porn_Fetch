2e96efdaf265750015a65e2c92d5839d53386afa
Johannes Habel <EchterAlsFake@proton.me>
2023-07-28 00:09:32 +0200

    1.5 Beta

--- widget.py
+++ widget.py
@@ -12,19 +12,24 @@ API by Egsagon: https://github.com/Egsagon/PHUB
 
 Author: EchterAlsFake - Johannes Habel
 
-Version: 1.4
+Version: 1.5
 2023
 """
 
 import sys
+import threading
+import os
+import argparse
+
+from PySide6 import QtCore
+from configparser import ConfigParser
+from colorama import *
+from tqdm import tqdm
 from PySide6.QtWidgets import QApplication, QWidget, QMessageBox, QTreeWidgetItem
 from PySide6.QtCore import Signal, QThreadPool, QRunnable, QObject
 from ui_form import Ui_Widget
 from phub import Client, Quality
-import os
-from PySide6 import QtCore
-from configparser import ConfigParser
-from colorama import *
+
 
 def ui_popup(text):
     qmsg_box = QMessageBox()
@@ -44,13 +49,355 @@ def test_path(path):
         return path
 
 
+class CLI():
+
+    def __init__(self):
+
+        self.z = Fore.LIGHTGREEN_EX + "[+]" + Fore.RESET
+        self.x = Fore.LIGHTRED_EX + "[~]" + Fore.RESET
+        self.pbar = None
+        self.quality = Quality.BEST
+        self.output_path = "./"
+        self.threading_mode = "multiple"
+        self.client = Client(language="en")
+        self.menu()
+
+    def callback(self, **kwargs):
+        def _update_progress(pos, total):
+            if self.pbar is None:
+                self.pbar = tqdm(total=total, dynamic_ncols=True)
+            self.pbar.update(pos - self.pbar.n)
+            if pos == total:
+                self.pbar.close()
+                self.pbar = None
+
+        return _update_progress
+
+    def check_path(self, path):
+        return True if os.path.exists(path) else False
+
+    def check_video(self, url):
+
+        try:
+            video = self.client.get(url)
+            return video
+
+        except Exception as e:
+            self.exception(e)
+
+    def exception(self, e):
+        print(self.z + f"Unhandled Exception: {e}")
+
+
+    def menu(self):
+
+        if self.quality == Quality.BEST:
+            quality_ext = Fore.LIGHTCYAN_EX + "Best"
+
+        elif self.quality == Quality.MIDDLE:
+            quality_ext = Fore.LIGHTMAGENTA_EX + "Middle"
+
+        elif self.quality == Quality.WORST:
+            quality_ext = Fore.LIGHTRED_EX + "Worst"
+
+        options = input(f"""
+{Fore.LIGHTYELLOW_EX}    Settings:
+{Fore.LIGHTWHITE_EX}|----------------------|
+{Fore.LIGHTCYAN_EX}|99) Change Quality     | {Fore.RESET}Currently set to: {quality_ext}
+{Fore.LIGHTMAGENTA_EX}|98) Set output path    {Fore.RESET}| Currently set to: {self.output_path}
+{Fore.LIGHTYELLOW_EX}|97) Set Threading mode | {Fore.RESET}Currently set to: {self.threading_mode}
+{Fore.RESET}|----------------------|
+
+
+1) Download a single Video
+2) Download videos from a file
+3) Download all videos from a User / Channel
+4) Get metadata from Videos
+5) Search for videos and download them
+6) Show credits
+7) Exit
+
+""")
+
+
+        if options == "99":
+            self.change_quality()
+
+        elif options == "98":
+            self.set_output_path()
+
+        elif options == "97":
+            self.set_threading_mode()
+
+        elif options == "1":
+            url = input(f"""
+{self.z}{Fore.LIGHTCYAN_EX}Please enter the video URL --->:""")
+            self.download_video(url)
+
+        elif options == "2":
+            self.download_from_file()
+
+        elif options == "3":
+            self.download_channel_user()
+
+        elif options == "4":
+            self.get_metadata()
+
+        elif options == "5":
+            self.search_videos()
+
+
+
+
+        elif options == "6":
+            pass
+
+        elif options == "7":
+            exit(0)
+
+
+
+
+
+
+
+
+
+    def change_quality(self):
+
+        options = """
+1) Change Quality to best
+2) Change Quality to middle
+3) Change Quality to worst
+4) Go back
+
+"""
+
+        if options == "1":
+            self.quality = Quality.BEST
+            print(self.z + "✓")
+
+        elif options == "2":
+            self.quality = Quality.MIDDLE
+            print(self.z + "✓")
+
+        elif options == "3":
+            self.quality = Quality.WORST
+            print(self.z + "✓")
+
+        elif options == "4":
+            self.menu()
+
+        else:
+            print(self.x + "Wrong option. Please enter in range 1-4")
+            self.change_quality()
+
+    def set_output_path(self):
+        output = input("""
+Please enter the new output path.  Make sure to put a / at the end -->:""")
+        self.output_path = output
+        print(self.z + "✓")
+
+    def set_threading_mode(self):
+        options = input("""
+1) Single threaded mode (downloads everything one by one)
+2) Multiple threaded mode (downloads multiple videos at once)
+3) Go back
+------------------------>:""")
+
+        if options == "1":
+            self.threading_mode = "single"
+
+        elif options == "2":
+            self.threading_mode = "multiple"
+
+
+        elif options == "3":
+            self.menu()
+
+    def raw_download(self, video):
+
+        title = video.title
+        print(self.z + Fore.LIGHTMAGENTA_EX + f"Downloading: {title}")
+
+        try:
+            video.download(path=self.output_path, callback=self.callback, quality=self.quality)
+            print(self.z + Fore.LIGHTBLUE_EX + "Download finished :) ")
+
+        except Exception as e:
+            self.exception(e)
+
+    def download_video(self, url):
+
+        mode = self.threading_mode
+        output_path = self.output_path
+        video = self.check_video(url)
+
+        if self.check_path(output_path):
+
+            if mode == "single":
+                self.raw_download(video)
+
+            elif mode == "multiple":
+
+                t = threading.Thread(target=self.raw_download, args=(video, ))
+                t.start()
+
+    def download_from_file(self):
+
+        print(f"""
+            {Fore.LIGHTCYAN_EX}INFORMATION
+
+{Fore.LIGHTWHITE_EX}The file needs to be in the following format:
+
+URL (After the URL, make a new line!
+URL
+URL etc....
+
+Do NOT seperate them with commas or anything else. Just with a new line. Should be easy enough :) 
+""")
+
+        file = input(f"""
+{self.z}{Fore.LIGHTYELLOW_EX}Path to URL file -->:""")
+
+
+        with open(file, "r") as url_file:
+            content = url_file.read().encode("utf-8").splitlines()
+            total_urls = len(content)
+
+            valid_urls = []
+
+            print(f"{self.z}{Fore.LIGHTCYAN_EX}Found: {total_urls}")
+            print(self.z + Fore.LIGHTMAGENTA_EX + "Verifying URLs...")
+
+            counter = 0
+
+            for url in tqdm(content):
+
+                counter += 1
+                try:
+                    self.check_video(url)
+                    valid_urls.append(url)
+
+                except Exception as e:
+                    print(self.x + Fore.RESET + f"URL: {url} is Invalid!")
+
+            print(self.z + Fore.LIGHTMAGENTA_EX + f"Downloading: {len(valid_urls)} Videos...")
+
+
+            for url in valid_urls:
+                self.download_video(url)
+
+    def download_channel_user(self):
+
+        user = input(f"""
+{self.z}{Fore.LIGHTMAGENTA_EX}Plese enter the URL to the User / Model / Channel Account -->:""")
+
+        user_object = self.client.get_user(user)
+
+        videos = user_object.videos
+        url_list = []
+        for video in videos:
+            url_list.append(video.url)
+
+        print(self.z + Fore.GREEN + f"Found: {len(url_list)} Videos.")
+        x = input(Fore.RESET + "Hit enter to download them all or B to go back")
+
+        if x == "B" or x == "b":
+            self.menu()
+
+        else:
+
+            for url in url_list:
+                self.download_video(url)
+
+    def get_metadata(self):
+
+        url = input(f"""
+{self.z}{Fore.LIGHTBLUE_EX}Please enter the URL of the video:""")
+
+        video = self.check_video(url)
+        print(self.z + Fore.LIGHTMAGENTA_EX + "Getting metadata....")
+
+        title = video.title
+        likes_up = video.like.up
+        likes_down = video.like.down
+        image_url = video.image_url
+        tags = video.tags
+        author = video.author
+        views = video.views
+        date = video.date
+        duration = video.duration
+        hotspots = video.hotspots
+
+        likes = f"Likes: {likes_up} | Dislikes: {likes_down}"
+
+        print(f"""
+{Fore.RESET}
+Title: {title}
+Rating: {likes}
+Image URL: {image_url}
+Tags: {tags}
+Author: {author}
+Views: {views}
+Date: {date}
+Duration: {duration}
+Hotspots: {hotspots}
+-- END --
+""")
+
+    def search_videos(self):
+
+        search_query = input(f"""
+{self.z}{Fore.RESET}Please enter your search query -->:""")
+
+        print(self.z + Fore.LIGHTMAGENTA_EX + "Searching....")
+
+        search = self.client.search(search_query)
+        urls = []
+
+        try:
+
+            for count, video in enumerate(search):
+                print(f"{count}) {video.title}")
+                urls.append(video.url)
+
+        except ConnectionError:
+            print("Got a connection error.  This is more a problem with PornHub, so you can ignore that. Restarting will fix it!")
+
+        downloads = input(Fore.RESET + "Enter the number of videos you want to download. Separate by comma e.g 1,7,12-->:")
+        videos = downloads.split(",")
+
+        for number in videos:
+            base_url = "https://www.pornhub.com/"
+            additional_url = urls[int(number)]
+            url = base_url + additional_url
+
+            self.client = Client()
+            self.download_video(url=url)
+
+
+
+
+
+
+
+
+    def show_credits(self):
+        pass
+    def exit(self):
+
+        print(Fore.LIGHTCYAN_EX + "Have a nice day :)")
+        exit(0)
+
+
 class DownloadProgressSignal(QObject):
     progress = Signal(int, int)
 
 class DownloadThread(QRunnable):
 
     def __init__(self, video, quality, output_path, parent=None):
-        super().__init__(parent)
+        super().__init__()
         self.video = video
         self.quality = quality
         self.output_path = output_path
@@ -331,6 +678,13 @@ class Widget(QWidget):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cli", help="CLI Terminal Version. Intended for advanced users or systems without a Graphical Environment", action="store_true")
+    args = parser.parse_args()
+
+    if args.cli:
+        CLI()
+
 
     app = QApplication(sys.argv)
     widget = Widget()
