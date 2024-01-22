308c81c4b3d336a1c3831c0d280612f718ce3d34
Johannes Habel <EchterAlsFake@proton.me>
2024-01-22 15:21:23 +0100

    iOS build script fixes

--- src/scripts/install_iSH.sh
+++ src/scripts/install_iSH.sh
@@ -1,4 +1,16 @@
-apk add git python3 py3-pip py3-virtualenv
+# We need to compile Python 3.11, because iSH uses 3.9 by default which is incompatible with PHUB
+apk add --no-cache build-base libffi-dev openssl-dev bzip2-dev zlib-dev readline-dev ncurses-dev gdbm-dev sqlite-dev
+wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tar.xz
+tar -xf Python-3.11.0.tar.xz
+cd Python-3.11.0
+./configure --enable-shared --enable-optimizations
+make
+make install
+
+cd ..
+rm -rf Python-3.11.0 Python-3.11.0.tar.xz
+apk del build-base libffi-dev openssl-dev bzip2-dev zlib-dev readline-dev ncurses-dev gdbm-dev sqlite-dev
+
 git clone https://github.com/EchterAlsFake/Porn_Fetch
 cd Porn_Fetch
 python3 -m venv venv
