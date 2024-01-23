c51f1fa9e01b53dd4406792afef453631329aea6
Johannes Habel <EchterAlsFake@proton.me>
2024-01-23 08:29:12 +0100

    testing the iOS script a bit more.
    # creating venv takes too much time on iOS, so we skip it.

--- src/scripts/install_iSH.sh
+++ src/scripts/install_iSH.sh
@@ -14,8 +14,6 @@ rm -rf Python-3.11.0 Python-3.11.0.tar.xz
 apk add git
 git clone https://github.com/EchterAlsFake/Porn_Fetch
 cd Porn_Fetch
-python3 -m venv venv
-source venv/bin/activate
 pip install -r requirements_cli.txt
 pip install pyinstaller
 pyinstaller -F Porn_Fetch_CLI.py
