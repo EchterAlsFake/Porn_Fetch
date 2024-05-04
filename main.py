2a9c176ab2e093e03e5c8777a3e295be373215cc
Johannes Habel <EchterAlsFake@proton.me>
2024-05-04 23:27:20 +0200

    - fixed the file emiting

--- main.py
+++ main.py
@@ -716,7 +716,7 @@ class AddUrls(QRunnable):
                 else:
                     ui_popup(invalid_input_string)
 
-        self.signals.total_progress.emit(idx, total)
+            self.signals.total_progress.emit(idx, total)
 
         self.signals.url_iterators.emit(iterator, model_iterators, search_iterators)
 
