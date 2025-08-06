44380ddce13aae53ac0940ae37835ffcca153000
Johannes Habel <EchterAlsFake@proton.me>
2025-08-06 16:07:52 +0200

    - updated windows build script #82

--- src/frontend/update.ps1
+++ src/frontend/update.ps1
@@ -5,37 +5,21 @@ Write-Output "Updating UI components, resource files, and translations..."
 Set-Location -Path $PSScriptRoot
 
 # Process UI files
-Write-Output "Processing UI files..."
-pyside6-uic form_android.ui -o ui_form_android.py
-pyside6-uic form_android_startup.ui -o ui_form_android_startup.py
-pyside6-uic form_desktop.ui -o ui_form_desktop.py
-pyside6-uic form_install_dialog.ui -o ui_form_install_dialog.py
-pyside6-uic form_license.ui -o ui_form_license.py
-pyside6-uic form_range_selector.ui -o ui_form_range_selector.py
-pyside6-uic form_keyboard_shortcuts.ui -o ui_form_keyboard_shortcuts.py
-pyside6-uic form_main_window.ui -o ui_form_main_window.py
+pyside6-uic UI/form_main_window.ui -o UI/ui_form_main_window.py
+pyside6-uic UI/form_android.ui -o UI/ui_form_android.py
+
 
 # Process resource file
 Write-Output "Processing resource file..."
-pyside6-rcc resources.qrc -o resources.py
+pyside6-rcc resources.qrc -o UI/resources.py
 
 # Update translations
 Write-Output "Updating translations..."
-pyside6-lupdate `
-    ../../main.py `
-    ../backend/class_help.py `
-    ui_form_desktop.py `
-    ui_form_install_dialog.py `
-    ui_form_license.py `
-    ui_form_range_selector.py `
-    ui_form_keyboard_shortcuts.py `
-    form_desktop.ui `
-    form_install_dialog.ui `
-    form_license.ui `
-    form_range_selector.ui `
-    form_keyboard_shortcuts.ui `
-    -ts translations/en.ts -no-obsolete
-
+# Translations
+pyside6-lupdate ../../main.py ../backend/class_help.py UI/form_main_window.ui UI/ui_form_main_window.py -ts translations/en.ts -no-obsolete
+pyside6-lupdate ../../main.py ../backend/class_help.py UI/form_main_window.ui UI/ui_form_main_window.py -ts translations/de_DE.ts -no-obsolete
+pyside6-lupdate ../../main.py ../backend/class_help.py UI/form_main_window.ui UI/ui_form_main_window.py -ts translations/fr.ts -no-obsolete
+pyside6-lupdate ../../main.py ../backend/class_help.py UI/form_main_window.ui UI/ui_form_main_window.py -ts translations/zh_CN.ts -no-obsolete
 pyside6-lrelease translations/de_DE.ts -qm translations/de_DE.qm
 pyside6-lrelease translations/zh_CN.ts -qm translations/zh_CN.qm
 pyside6-lrelease translations/fr.ts -qm translations/fr.qm
